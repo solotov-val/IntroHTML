9264c3481c7afe22812657f2d2cf77597cd2012c
Priller Patrick <prillerpatrick@gmail.com>
2023-09-18 12:08:33 +0200

    i dont know what i am doing

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function Function() {
+  let x = document.getElementById("topnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
